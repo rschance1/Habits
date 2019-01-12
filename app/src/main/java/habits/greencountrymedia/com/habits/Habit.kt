ee47d46370194307fac8ed809a8965207b1c77c0
rschance1 <richardchance1980@gmal.com>
2019-01-11 21:54:37 -0600

    cleaned up data class

--- app/src/main/java/habits/greencountrymedia/com/habits/Habit.kt
+++ app/src/main/java/habits/greencountrymedia/com/habits/Habit.kt
@@ -3,14 +3,3 @@ package habits.greencountrymedia.com.habits
 import android.graphics.Bitmap
 
 data class Habit(val title: String, val description: String, val image: Bitmap)
-
-//fun getSampleHabits(): List<Habit> {
-//    return listOf(
-//        Habit("Go for a walk",
-//            "A nice walk in the sun get you ready for the day ahead",
-//            R.drawable.walk),
-//        Habit("Drink some water",
-//            "A refreshing glass of water keeps you hydrated",
-//            R.drawable.water)
-//    )
-//}
